9f1cedd631b3474be7458b0fe6a86826384799d1
marksakurada <mark.sakurada@gmail.com>
2025-11-18 15:24:27 -0500

    Add project name environment variable

--- setup/init.sh
+++ setup/init.sh
@@ -5,6 +5,7 @@ export TF_VAR_cc_cloud_api_secret="<Confluent Cloud API Secret>"
 export TF_VAR_mongodbatlas_public_key="<MongoDB Public API Key>"
 export TF_VAR_mongodbatlas_private_key="<MongoDB Private API Key>"
 export TF_VAR_mongodb_atlas_org_id="<MongoDB Organization ID>"
+export TF_VAR_project_name="<username>"
 export AWS_DEFAULT_REGION="us-west-2" #If using a Confluent-provided AWS account, make sure this region matches the region found in the above steps (most likely it will be us-west-2)
 export AWS_REGION="us-west-2" #If using a Confluent-provided AWS account, make sure this region matches the region found in the above steps (most likely it will be us-west-2)
 export AWS_ACCESS_KEY_ID="<AWS Access Key ID"
